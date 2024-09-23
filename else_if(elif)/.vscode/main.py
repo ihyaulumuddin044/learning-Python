304df5b1e399e768789014175e184158da6d8293
ihyaulumuddin044 <ihya400116@gmail.com>
2024-09-23 20:21:07 +0700

    else if dan operasi logika

---
+++ else_if(elif)/.vscode/main.py
@@ -0,0 +1,14 @@
+# else if statement (elif)
+
+nama = input("Masukkan nama Anda! ")
+
+if nama == "ucup": # kondisi 1
+    print("haloooo cuppppppp") # kondisi true 1
+elif nama == "otong" : # kondisi 2 
+    print("tong bayar tong") # kondisi true 2
+elif nama == "kokong" : # kondisi 3
+    print("kong bayar tong") # kondisi true 3
+else:
+    print("luu  olang siapa?") # aksi false
+
+print("ini adalah akhir dari program")
