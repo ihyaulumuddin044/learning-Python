fee83c3cf289558a95f673aa77e860273be4e5ec
ihyaulumuddin044 <ihya400116@gmail.com>
2024-09-23 20:23:11 +0700

    while

---
+++ while/.vscode/main.py
@@ -0,0 +1,13 @@
+# while loop
+# while kondisi:
+#        aksi ini
+#        aksi itu
+angka = 0
+print(f"angka sekarang adala{angka}")
+
+while angka < 5 :
+    angka += 1
+    print(f"angka sekarang adalah --> {angka}") 
+
+print("cukupppppp")
+
